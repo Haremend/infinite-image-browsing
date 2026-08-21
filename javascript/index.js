4a46a0a40c41368a6be6f07032ae21a5f8430c69
Zhangcy <zhangcy@boss.com.cn>
2026-08-21 18:09:46 +0800

    插件页面地址更新

--- javascript/index.js
+++ javascript/index.js
@@ -13,7 +13,7 @@ Promise.resolve().then(async () => {
     <link rel="icon" href="/favicon.ico" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Infinite Image Browsing</title>
-    <script type="module" crossorigin src="/infinite_image_browsing/fe-static/assets/index-b91a4c5d.js"></script>
+    <script type="module" crossorigin src="/infinite_image_browsing/fe-static/assets/index-81a7db24.js"></script>
     <link rel="stylesheet" href="/infinite_image_browsing/fe-static/assets/index-987ba8f4.css">
   </head>
 
